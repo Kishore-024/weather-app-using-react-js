4e1e592ade912cc5edf7e8e4827b1a84dedf9307
Kishore-024 <139609921+Kishore-024@users.noreply.github.com>
2023-11-07 16:34:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,50 +1,14 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-const KEY="7cd2a1d82a8721c453fbcef061d5246d"
+import './App.css';
+import Weather from './Weather';
 
-const Weather = () => {
-    const[city,setCity]=useState()
-    const [weatherData, setWeatherData] = useState(null);
-const change=(e)=>{
-    setCity(e.target.value)
-}
-    
-const handleChange=async()=>{
-    try{
-        const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`
-        )
-        setWeatherData(response.data);}
-    catch(error){
-        
-
-
-    }
-
-}
+function App() {
   return (
-    <div>
-        <h1> Weather App</h1>
-        <input
-        type='text'
-        placeholder='Enter the city name'
-        value={city}
-        onChange={change}
-        />
-        <button onClick={handleChange}>Fetch data</button>
-        {weatherData && (
-        <div>
-          <h2>{weatherData.name}</h2>
-          <p>Temperature: {weatherData.main.temp /8}°C</p>
-          <p>Climatic condition: {weatherData.weather[0].description}</p>
-          <p>Wind speed: {weatherData.wind.speed}</p>
-          <p> Latitude: {weatherData.coord.lat}</p>
-          <p> Longitude: {weatherData.coord.lon}</p>
-          </div>)}
+    <div className="App">
+      
+      <Weather/>
       
     </div>
-  )
+  );
 }
 
-export default Weather
-
+export default App;
